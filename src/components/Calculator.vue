7d25017ea6cef90ed581986fa6f6bd9f880ce867
= <=>
2025-02-12 11:40:54 +0100

    init commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,13 @@
+<template>
+ <div class="px-2" style="max-width: 400px; margin: 40px auto; background: #234">
+ <div class="w-full rounded-sm mt-1 mb-1 px-2 text-right font-bold text-white">
+    
+ </div>
+ </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: { "bg-vue-dark": '#31475e',
+      "bg-vue-hover": "#305875",
+      "bg-vue-green": "3fb984"
+    },
+  },
+  plugins: [],
+}
+

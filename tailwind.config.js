b95b338cd41deeaa105772dc8f1d96de55b9b589
= <=>
2025-02-12 12:59:49 +0100

    add changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,7 @@
 export default {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: { "bg-vue-dark": '#31475e',
-      "bg-vue-hover": "#305875",
-      "bg-vue-green": "3fb984"
-    },
+    extend: {},
   },
   plugins: [],
 }

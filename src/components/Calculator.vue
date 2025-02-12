80162f87e0d981be17e5c753bf45fbb6d23acde4
= <=>
2025-02-12 11:52:28 +0100

    add: new elements

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,13 +1,22 @@
 <template>
- <div class="px-2" style="max-width: 400px; margin: 40px auto; background: #234">
- <div class="w-full rounded-sm mt-1 mb-1 px-2 text-right font-bold text-white">
-    
- </div>
- </div>
+    <div class="px-2" style="max-width: 400px; margin: 40px auto; background: #234">
+        <div class="w-full rounded-sm mt-1 mb-1 px-2 text-right font-bold text-white">
+            {{ calculatorValue || 0 }}
+        </div>
+
+        <div class="row"></div>
+    </div>
+
+
 </template>
 
 <script setup>
+import { defineprops } from 'vue'
+import { ref } from 'vue'
+
+// const data = ref{ []}
+calculatorValue: ''
+calculatorElements: ['C', '*', '/', '-', '7', '8', '9', '+', '4', '5', '6', '%', '1', '2', '3', '=', '0', '.']
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
